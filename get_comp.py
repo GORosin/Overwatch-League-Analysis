86108fb78e360d4310ac411663390ced8ae8bbfc
Guy Rosin <guy.rosin@cern.ch>
2019-03-06 16:43:20 -0500

    extracted hero breakdown

--- get_comp.py
+++ get_comp.py
@@ -1,5 +1,9 @@
 import  bs4
 import requests
+import re
+import json
+import ast
+
 print("************************************************")
 print("IMAGINATION IS THE ESSENCE OF DISCOVERY")
 print("************************************************")
@@ -8,16 +12,16 @@ match_urls=["https://www.winstonslab.com/matches/match.php?id=40"+str(i) for i i
 
 match_url=match_urls[0]
 html_data=requests.get(match_url)
+print(match_url)
 html_data.raise_for_status()
-soup= bs4.BeautifulSoup(html_data.text,features="lxml")
-rein_bar=0
-winston_bar=0
-sombra_bar=0
-ana_bar=0
-hammon_bar=0
-mercy_bar=0
-orisa_bar=0
-#print(soup)
-elems=soup.find_all("div",class_="progress-bar bar-hero bar-* bar-dataset1")
-print(elems)
 
+parsed_html=[line for line in html_data.text.split('\n') if 'heroStatsArr.concat' in line]
+data=re.split(r"\(|\)",parsed_html[0])[1][1:-1]
+players_tuple=ast.literal_eval(data)
+for dic in players_tuple:
+    print("-------------------------")
+    print(f"team:{dic['teamName']}")
+    print(f"player name:{dic['playerName']}")
+    print(f"map : {dic['gameNumber']} -- {dic['map']}")
+    print(f"hero :  {dic['hero']}")
+    print(f"time played {int(dic['timePlayed']) // 60}:{int(dic['timePlayed'])% 60}")
