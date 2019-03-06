a91ee2592e9933340bec7cca75c6c59b799119c4
Guy Rosin <guy.rosin@cern.ch>
2019-03-06 17:45:33 -0500

    3.5 friendly

--- get_comp.py
+++ get_comp.py
@@ -10,9 +10,8 @@ print("************************************************")
 
 match_urls=["https://www.winstonslab.com/matches/match.php?id=40"+str(i) for i in range(58,110)]
 
-match_url=match_urls[0]
+match_url=match_urls[13]
 html_data=requests.get(match_url)
-print(match_url)
 html_data.raise_for_status()
 
 parsed_html=[line for line in html_data.text.split('\n') if 'heroStatsArr.concat' in line]
@@ -20,8 +19,8 @@ data=re.split(r"\(|\)",parsed_html[0])[1][1:-1]
 players_tuple=ast.literal_eval(data)
 for dic in players_tuple:
     print("-------------------------")
-    print(f"team:{dic['teamName']}")
-    print(f"player name:{dic['playerName']}")
-    print(f"map : {dic['gameNumber']} -- {dic['map']}")
-    print(f"hero :  {dic['hero']}")
-    print(f"time played {int(dic['timePlayed']) // 60}:{int(dic['timePlayed'])% 60}")
+    print("team:"+str(dic['teamName']))
+    print("player name:"+str(dic['playerName']))
+    print("map : "+str(dic['gameNumber'])+" -- "+str(dic['map']))
+    print("hero :  "+str(dic['hero']))
+    print("time played "+str(int(dic['timePlayed']) // 60)+":"+str(int(dic['timePlayed'])% 60))
