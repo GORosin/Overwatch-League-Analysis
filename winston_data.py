e1d5d524d9605542ebdd522fc252f68573d4b806
GORosin <oxr3231@g.rit.edu>
2019-04-08 00:01:57 -0400

    reverted to version prior to dictionary split

--- winston_data.py
+++ winston_data.py
@@ -1,180 +1,174 @@
 
-flex_dps={
+dps_players={
     "Ado":"WAS",
     "Adora":"HZS",
     "Agilities":"VAL",
+    "aKm":"DAL",
     "Apply":"FLA",
     "Architect":"SFS",
+    "Baconjack":"CDH",
     "Bazzi":"HZS",
+    "BIRDRING":"LDN",
     "blase":"BOS",
+    "bqb":"FLA",
+    "carpe":"PHI",
+    "ColourHex":"BOS",
+    "Corey":"WAS",
+    "dafran":"ATL",
+    "Danteh":"HOU",
     "DDing":"SHD",
+    "Decay":"GLA",
+    "diem":" SHD",
+    "EFFECT":"DAL",
     "Eileen":"GZC",
     "eqo":"PHI",
     "Erster":"ATL",
+    "Fleta":"SEO",
+    "GodsB":"HZS",
     "Guard":"LDN",
     "Haksal":"VAN",
+    "Happy":"GZC",
     "Hydration":"GLA",
+    "ivy":"TOR",
     "Jake":"HOU",
     "JinMu":"CDH",
+    "KariV":"VAL",
     "KSF":"VAL",
     "Kyb":"GZC",
     "Libero":"NYE",
+    "LiNkzr":"HOU",
     "Munchkin":"SEO",
+    "Nenne":"NYE",
     "NiCOgdh":"PAR",
+    "NLaaeR":"ATL",
+    "Profit":"LDN",
     "Rascal":"SFS",
+    "Saebyeolbe":"NYE",
+    "sayaplayer":"FLA",
+    "SeoMinSoo":"VAN",
+    "sinatraa":"SFS",
     "snillo":"PHI",
+    "SoOn":"PAR",
     "Stellar":"TOR",
+    "Stitch":"VAN",
     "Stratus":"WAS",
+    "STRIKER":"SFS",
+    "Surefour":"GLA",
     "TviQ":"FLA",
     "YOUNGJIN":"SHD",
-    "ZachaREEE":"DAL"
+    "ZachaREEE":"DAL",
 }
 
-main_dps={
-    "BIRDRING": "LDN",
-    "aKm":"DAL",
-    "Baconjack":"CDH",
-    "bqb": "FLA",
-    "carpe": "PHI",
-    "ColourHex": "BOS",
-    "Corey": "WAS",
-    "dafran": "ATL",
-    "Danteh": "HOU",
-    "Decay": "GLA",
-    "diem": " SHD",
-    "EFFECT": "DAL",
-    "Fleta": "SEO",
-    "GodsB": "HZS",
-    "Happy": "GZC",
-    "ivy": "TOR",
-    "KariV": "VAL",
-    "LiNkzr": "HOU",
-    "Nenne": "NYE",
-    "NLaaeR": "ATL",
-    "Profit": "LDN",
-    "Saebyeolbe": "NYE",
-    "sayaplayer": "FLA",
-    "SeoMinSoo": "VAN",
-    "sinatraa": "SFS",
-    "SoOn": "PAR",
-    "Stitch": "VAN",
-    "STRIKER": "SFS",
-    "Surefour": "GLA",
-}
-
-flex_supports={
-    "AimGod":"BOS",
-    "Bdosin":"LDN",
-    "BEBE":"HZS",
-    "Boombox":"PHI",
-    "Dogman":"ATL",
-    "Gido":"WAS",
-    "HaGoPeun":"FLA",
-    "HyP":"PAR",
-    "IZaYaKI":"VAL",
-    "JJONAK":"NYE",
-    "Kodak":"ATL",
-    "Kyo":"CDH",
-    "Luffy":"SHD",
-    "Neko":"TOR",
-    "RAPEL":"VAN",
-    "Rawkus":"HOU",
-    "Revenge":"HZS",
-    "ryujehong":"SEO",
-    "Shaz":"GLA",
-    "shu":"GZC",
-    "sleepy":"SFS",
-    "Twilight":"VAN",
-    "uNKOE":"DAL",
-    "Viol2t":"SFS",
-}
-
-main_supports={
+support_players={
     "Aid":"TOR",
+    "AimGod":"BOS",
     "alemao":"BOS",
     "Anamo":"NYE",
     "ArK":"NYE",
     "Bani":"HOU",
+    "Bdosin":"LDN",
+    "BEBE":"HZS",
     "BigGoose":"GLA",
     "Boink":"HOU",
-    "Chara": "GZC",
-    "Closer": "DAL",
-    "CoMa": "SHD",
-    "Custa": "VAL",
-    "Elk": "PHI",
-    "Fahzix": "WAS",
-    "Hyeonu": "WAS",
-    "iDK": "HZS",
-    "Jecse": "SEO",
-    "Kellex": "BOS",
-    "Kris": "FLA",
-    "Kruise": "PAR",
-    "Masaa": "ATL",
-    "moth": "SFS",
-    "neptuNo": "PHI",
-    "NUS": "LDN",
-    "RoKy": "TOR",
-    "SLIME": "VAN",
-    "Yveltal": "CDH",
+    "Boombox":"PHI",
+    "Chara":"GZC",
+    "Closer":"DAL",
+    "CoMa":"SHD",
+    "Custa":"VAL",
+    "Dogman":"ATL",
+    "Elk":"PHI",
+    "Fahzix":"WAS",
+    "Gido":"WAS",
+    "HaGoPeun":"FLA",
+    "Hyeonu":"WAS",
+    "HyP":"PAR",
+    "iDK":"HZS",
+    "IZaYaKI":"VAL",
+    "Jecse":"SEO",
+    "JJONAK":"NYE",
+    "Kellex":"BOS",
+    "Kodak":"ATL",
+    "Kris":"FLA",
+    "Kruise":"PAR",
+    "Kyo":"CDH",
+    "Luffy":"SHD",
+    "Masaa":"ATL",
+    "moth":"SFS",
+    "Neko":"TOR",
+    "neptuNo":"PHI",
+    "NUS":"LDN",
+    "RAPEL":"VAN",
+    "Rawkus":"HOU",
+    "Revenge":"HZS",
+    "RoKy":"TOR",
+    "ryujehong":"SEO",
+    "Shaz":"GLA",
+    "shu":"GZC",
+    "sleepy":"SFS",
+    "SLIME":"VAN",
+    "Twilight":"VAN",
+    "uNKOE":"DAL",
+    "Viol2t":"SFS",
+    "Yveltal":"CDH",
 }
 
-flex_tanks={
+tank_players={
+    "ameng":"CDH",
+    "Axxiom":"BOS",
+    "BenBest":"PAR",
+    "BUMPER":"VAN",
     "Choihyobin":"SFS",
     "coolmatt":"HOU",
     "Daco":"ATL",
     "Elsa":"CDH",
     "Envy":"TOR",
+    "Fate":"VAL",
     "Finnsi":"PAR",
+    "Fissure":"SEO",
     "Fury":"LDN",
+    "Fusions":"BOS",
+    "Gamsu":"SHD",
+    "Gator":"ATL",
     "Geguri":"SHD",
+    "Gesture":"LDN",
+    "Guxue":"HZS",
     "HOTBA":"GZC",
+    "Janus":"WAS",
     "JJANU":"VAN",
+    "KuKi":"VAL",
     "lateyoung":"CDH",
+    "Mano":"NYE",
     "Mcgravy":"FLA",
     "MekO":"NYE",
     "Michelle":"SEO",
+    "Muma":"HOU",
     "Nevix":"SFS",
+    "NoSmite":"HZS",
     "NotE":"BOS",
+    "OGE":"DAL",
     "Poko":"PHI",
+    "Pokpo":"ATL",
     "rCk":"DAL",
     "Ria":"HZS",
+    "Rio":"GZC",
+    "rOar":"GLA",
+    "Sado":"PHI",
     "Sansam":"WAS",
+    "Smurf":"SFS",
     "SPACE":"VAL",
     "SPREE":"HOU",
+    "super":"SFS",
+    "SWoN":"FLA",
     "Void":"GLA",
     "xepheR":"FLA",
-}
-
-main_tanks={
-    "ameng": "CDH",
-    "Axxiom": "BOS",
-    "BenBest": "PAR",
-    "BUMPER": "VAN",
-    "Fate": "VAL",
-    "Fissure": "SEO",
-    "Fusions": "BOS",
-    "Gamsu": "SHD",
-    "Gator": "ATL",
-    "Gesture": "LDN",
-    "Guxue": "HZS",
-    "Janus": "WAS",
-    "KuKi": "VAL",
-    "Mano": "NYE",
-    "Muma": "HOU",
-    "NoSmite": "HZS",
-    "OGE": "DAL",
-    "Pokpo": "ATL",
-    "Rio": "GZC",
-    "rOar": "GLA",
-    "Sado": "PHI",
-    "Smurf": "SFS",
-    "super": "SFS",
-    "SWoN": "FLA",
-    "Yakpung": "TOR",
+    "Yakpung":"TOR",
 }
 
 #lower case versions of dictionaries for key searching
-
+damage = dict((key.lower(),value) for key,value in dps_players.items())
+support = dict((key.lower(),value) for key,value in support_players.items())
+tank = dict((key.lower(),value) for key,value in tank_players.items())
+all_players={**damage,**support,**tank}
 
 import bs4
 import requests
@@ -184,13 +178,7 @@ import ast
 
 #format of data list of maps
 #each maps has a dictionary of {map:map_name, player 1:[(hero,time),(hero,time)], player 2:[(hero,time),(hero,time)]...}
-l_main_dps={k.lower():v for k,v in main_dps.items()}
-l_flex_dps={k.lower():v for k,v in flex_dps.items()}
-l_main_tanks={k.lower():v for k,v in main_tanks.items()}
-l_flex_tanks={k.lower():v for k,v in flex_tanks.items()}
-l_main_supports={k.lower():v for k,v in main_supports.items()}
-l_flex_supports={k.lower():v for k,v in flex_supports.items()}
-all_players={**l_main_dps,**l_flex_dps,**l_main_tanks,**l_flex_tanks,**l_main_supports,**l_flex_supports}
+
 def sort_winstons_data(players_tuple):
     match_rounds=[{},{},{},{},{}]
     for dic in players_tuple:
@@ -229,7 +217,7 @@ def calculate_comp(hero_times):
     dive=hero_times['Winston']-winston_goats
     sombra_goats=hero_times['Zarya']-hero_times['D.Va'] if hero_times['Zarya']>hero_times['D.Va'] else 0
     savta_goats=hero_times['Zarya']-hero_times['Brigitte'] if hero_times['Zarya']>hero_times['Brigitte'] else 0
-    #I wasn't cut out for retirement anyways.	
+    #I wasn't cut out for retirement anyways.
     total_dps=(hero_times['Widowmaker']+hero_times['Tracer'])/2
     wrecking_crew=total_dps-dive
     return {"Dive":round(dive,3),
@@ -239,30 +227,25 @@ def calculate_comp(hero_times):
             "Savta Goats":round(savta_goats,3),
             "Wrecking_Crew":round(wrecking_crew,3)}
 
-def write_comp(comp,csv):
-    with open(csv,'a') as sheet:
-        csv.write(comp['Goats']+",")
-        csv.write(comp['Dive']+",")
-        csv.write(comp['Winston_Goats']+",")
-        csv.write(comp['Sombra_Goats']+",")
-        csv.write(comp['Savta_Goats']+",")
-        csv.write(comp['Wrecking_Crew']+",")
-        
-def team_stats(side,data,csv):
+def team_stats(side,data,csv,map=0):
     elems=soup.find_all("table",class_="table table-striped "+side+" sortable-table match")
-    players = [[],[],[],[],[],[]]
+    players = {}
     with open (csv,'a') as sheet:
         for i, el in enumerate(elems):
-            '''
-            if i == 0:
-                print("************************************************")
-                print("Match Stats:")
-                print("************************************************")
+            if map == 0:
+                if i == 0:
+                    print("************************************************")
+                    print("Match Stats:")
+                    print("************************************************")
+                else:
+                    print("************************************************")
+                    print("Map "+str(i)+" Stats:")
+                    print("************************************************")
             else:
-                print("************************************************")
-                print("Map "+str(i)+" Stats:")
-                print("************************************************")
-            '''
+                if i == map:
+                    print("************************************************")
+                    print("Map " + str(i) + " Stats:")
+                    print("************************************************")
             names = el.find_all("a")
             killdeathratio = el.find_all("td", class_="center page1")
             kills = []
@@ -277,39 +260,36 @@ def team_stats(side,data,csv):
                     deaths.append(kdr)
             ults = el.find_all("td", class_="center page1 not-in-small")
             for index, name in enumerate(names):
-                if str(name.contents[0]).lower() in l_main_dps and (not bool( players[0])):
-                    players[0] = {"name":name.contents[0],"data":[kills[index].contents[0], deaths[index].contents[0],
-                                                      ults[index].contents[0], kdd[index].contents[0].strip()]}
+                if str(name.contents[0]).lower() in damage:
+                    players[str(name.contents[0])] = [kills[index].contents[0], deaths[index].contents[0],
+                                                      ults[index].contents[0], kdd[index].contents[0].strip(),"Damage",damage[str(name.contents[0]).lower()]]
                     #print("Role: Damage")
-                elif (str(name.contents[0]).lower() in l_flex_dps) or (str(name.contents[0]).lower() in l_main_dps):
-                    players[1] = {"name":name.contents[0],"data":[kills[index].contents[0], deaths[index].contents[0],
-                                                      ults[index].contents[0], kdd[index].contents[0].strip()]}
+                elif str(name.contents[0]).lower() in support:
+                    players[str(name.contents[0])] = [kills[index].contents[0], deaths[index].contents[0],
+                                                      ults[index].contents[0], kdd[index].contents[0].strip(), "Support",support[str(name.contents[0]).lower()]]
                     #print("Role: Support")
-                elif str(name.contents[0]).lower() in l_main_tanks:
-                    players[2] = {"name":name.contents[0],"data":[kills[index].contents[0], deaths[index].contents[0],
-                                                      ults[index].contents[0], kdd[index].contents[0].strip()]}
-                elif str(name.contents[0]).lower() in l_flex_tanks:
-                    players[3] = {"name":name.contents[0],"data":[kills[index].contents[0], deaths[index].contents[0],
-                                                      ults[index].contents[0], kdd[index].contents[0].strip()]}
-                elif str(name.contents[0]).lower() in l_main_supports:
-                    players[4] = {"name":name.contents[0],"data":[kills[index].contents[0], deaths[index].contents[0],
-                                                      ults[index].contents[0], kdd[index].contents[0].strip()]}
-                elif str(name.contents[0]).lower() in l_flex_supports:
-                    players[5] = {"name":name.contents[0],"data":[kills[index].contents[0], deaths[index].contents[0],
-                                                      ults[index].contents[0], kdd[index].contents[0].strip()]}
+                elif str(name.contents[0]).lower() in tank:
+                    players[str(name.contents[0])] = [kills[index].contents[0], deaths[index].contents[0],
+                                                      ults[index].contents[0], kdd[index].contents[0].strip(), "Tank",tank[str(name.contents[0]).lower()]]
                     #print("Role: Tank")
-            roles={0:"main dps",1:"flex dps",2:"main tank",3:"flex tank",4:"main support",5:"flex support"}
-        for player in range(len(players)):
-            print("Name: " + players[player]['name'])
-            print("Role " +roles[player])
-            print("Kills: " + str(players[player]['data'][0]))
-            print("Deaths: " + str(players[player]['data'][1]))
-            print("Ults: " + str(players[player]['data'][2]))
-            print("First Kills - Deaths: "+ str(players[player]['data'][3]))
-            print("-----------------------------")
-                
-            #sheet.write(str(players[player][5]) + "," + str(players[player][4])+","+str(players[player][0])+","+str(players[player][1])+"," +str(players[player][2])+","+str(players[player][3])+",")
-            #sheet.write('\n')
+            for player in players:
+                if player in data[i-1] or i == 0 :
+
+                    print("Name: " + str(player))
+                    if player.lower() in damage:
+                        print("Role: Damage")
+                    elif player.lower() in support:
+                        print("Role: Support")
+                    elif player.lower() in tank:
+                        print("Role: Tank")
+                    print("Kills: " + str(players[player][0]))
+                    print("Deaths: " + str(players[player][1]))
+                    print("Ults: " + str(players[player][2]))
+                    print("First Kills - Deaths: "+ str(players[player][3]))
+
+                    sheet.write(str(i)+","+str(players[player][5]) + "," + str(players[player][4])+","+str(players[player][0])+","+str(players[player][1])+","
+                                +str(players[player][2])+","+str(players[player][3]))
+            sheet.write('\n')
 
 def round_map_data(data):
     for round_map in data:
@@ -322,8 +302,6 @@ def round_map_data(data):
 def collect_match_data(data):
     csv = "winston_data.csv"
     team_stats("left-side", data, csv)  # away team
-    #hero_times=hero_play_time(data[i],'LDN')
-    #write_comp(calculate_comp(hero_times))
     team_stats("right-side", data, csv)  # home team
     with open(csv, 'a') as sheet:
         sheet.write('\n')
@@ -345,5 +323,7 @@ if __name__ == '__main__':
     players_tuple = ast.literal_eval(data)
     sorted_data= sort_winstons_data(players_tuple)
 
-
-    collect_match_data(sorted_data[1])
+    #hero_times=hero_play_time(sorted_data[2],'LDN')
+    #print(calculate_comp(hero_times))
+    #get_comp(players_tuple)
+    collect_match_data(sorted_data)
