66f4f89566968842be372e33af6581cc5e8b6c42
Guy Rosin <guy.rosin@cern.ch>
2019-05-02 11:43:27 -0400

    added pca

--- owl_nn.py
+++ owl_nn.py
@@ -6,14 +6,15 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 import pandas as pd
 from keras.optimizers import Adagrad,Adam
+from sklearn.decomposition import PCA
 
 teams={"PHI":1,"LDN":2,"BOS":3,"NYE":4,"PAR":5,"GLA":6,"SHD":7,"DAL":8,"CDH":9,"VAL":10,"SEO":11,"HZS":12,"GZC":13,"TOR":14,"WAS":15,"HOU":16,"ATL":17,"FLA":18,"SFS":19,"VAN":20}
 
-def train(x,y):
-    nn_win_model=create_model()
+def train(x,y,shape):
+    nn_win_model=create_model(shape)
     print(x.shape)
     print(y.shape)
-    nn_win_model.fit(x,y,epochs=300, batch_size=30)
+    nn_win_model.fit(x,y,epochs=200, batch_size=30)
     return nn_win_model
 
 def one_hot_encode(value,size=20):
@@ -21,35 +22,32 @@ def one_hot_encode(value,size=20):
     arr[teams[value]-1]=1
     return arr
 
-def create_model():
+def create_model(shape):
     model=Sequential()
-    model.add(Dense(60,input_dim=60,kernel_initializer='normal',activation='sigmoid'))
-    model.add(Dense(70,activation='sigmoid'))
-    model.add(Dense(30,activation='sigmoid'))
-    model.add(Dense(15,activation='sigmoid'))
+    model.add(Dense(shape,input_dim=shape,kernel_initializer='normal',activation='sigmoid'))
+    model.add(Dense(6,activation='sigmoid'))
     model.add(Dense(2,kernel_initializer='normal',activation='softmax'))
-    opt=Adam(lr=0.1,decay=0.12)
+    opt=Adam(lr=0.04,decay=0.01)
     model.compile(loss='categorical_crossentropy',optimizer=opt)
     return model
 
-def accuracy(trained_model,data,labels,teams):
+def accuracy(trained_model,data,labels):
     correct=0
     indx=0
+    results=0
     for datum,label in zip(data,labels):
         prediction=trained_model.predict(datum.reshape(1,-1))[0]
         pred_winner=int(prediction[0] > prediction[1])
         actual_winner=int(label[0] > label[1])
         if pred_winner==actual_winner:
             correct+=1
-        else:
-            pass
-            print(prediction)
-            print(label)
-            print(teams.ix[indx,:])
+        print(prediction)
+        results+=(prediction[0]-label[0])**2
+        print(label)
         indx+=1
     print("correct: "+str(correct))
     print("total: "+str(indx))
-    
+    return results
 def create_labels():
     score_data=pd.read_csv("stage1_results.csv")
     team_data=pd.read_csv("winston_data.csv")
@@ -70,7 +68,23 @@ def create_labels():
         idx+=1
 
     return X,y,score_data.loc[:,"teamA":"teamB"]
+def main_test(n_comp):
+    reduce_data=PCA(n_comp)
+    X_full,y,teams=create_labels()
+    X=reduce_data.fit_transform(X_full)
+    ties=[6, 77, 106, 131, 156, 231, 239, 260, 265]
+    train_data=np.ones(len(y),dtype=bool)
+    test_data=np.zeros(len(y),dtype=bool)
+    for idx in ties:
+        train_data[idx]=False
+        test_data[idx]=True
+    
+    x_train=X[0:200]
+    y_train=y[0:200]
+    x_test=X[200:]
+    y_test=y[200:]
+    model=train(x_train,y_train,n_comp)
+    results=accuracy(model,x_test,y_test)
+    return results
 
-X,y,teams=create_labels()
-model=train(X[:200],y[:200])
-#accuracy(model,X[200:],y[200:],teams)
+print(main_test(4))
